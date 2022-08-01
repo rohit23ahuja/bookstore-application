694602cb81a54ab2c3f6e476668a9912e88579e2
rohitahuja <rohit23ahuja@gmail.com>
2022-08-01 20:43:06 +0530

    Database migrations using liquibase

---
+++ src/main/resources/db/changelog/changes/v0001.sql
@@ -0,0 +1,17 @@
+create table book
+(
+  book_id BIGSERIAL PRIMARY KEY,
+  isbn varchar(255) not null,
+  title varchar(2000) not null,
+  author varchar(100) not null,
+  price integer not null, 
+  version integer default 1 not null,
+  quantity integer
+);
+
+INSERT INTO BOOK (ISBN, TITLE, AUTHOR, PRICE,VERSION, QUANTITY) VALUES ('123ABC','Game Of Thrones','George Martin',1700,1,5);
+INSERT INTO BOOK (ISBN, TITLE, AUTHOR, PRICE,VERSION, QUANTITY) VALUES ('234DEF','Lord Of Rings','Donot know',3000,1,5);
+INSERT INTO BOOK (ISBN, TITLE, AUTHOR, PRICE,VERSION, QUANTITY) VALUES ('345EFG','Homo Sapiens','Yuval Harari',500,1,5);
+INSERT INTO BOOK (ISBN, TITLE, AUTHOR, PRICE,VERSION, QUANTITY) VALUES ('456FGH','optio reprehenderit','Rohit A',250,1,5);
+INSERT INTO BOOK (ISBN, TITLE, AUTHOR, PRICE,VERSION, QUANTITY) VALUES ('567FGH','reprehenderit dolor','Rohit A',250,1,5);
+INSERT INTO BOOK (ISBN, TITLE, AUTHOR, PRICE,VERSION, QUANTITY) VALUES ('678FGH','sit autem','Rohit A',250,1,5);
